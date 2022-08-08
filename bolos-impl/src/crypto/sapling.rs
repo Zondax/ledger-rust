ad5c606ae6846180ac614477eb345113b62a8ba4
becominginsane <francesco@zondax.ch>
2022-08-08 13:53:56 -0700

    feat(mock): sapling seed
    style: formatting

--- bolos-impl/src/crypto/sapling.rs
+++ bolos-impl/src/crypto/sapling.rs
@@ -1,78 +1,30 @@
 use super::{bip32::BIP32Path, Curve, Mode, CHAIN_CODE_LEN};
 use jubjub;
 
-pub const CTX_EXPAND_SEED_HASH_LEN : size_t = 64;
-pub const CTX_EXPAND_SEED_LEN : size_t = 16;
+pub const CTX_EXPAND_SEED_HASH_LEN: size_t = 64;
+pub const CTX_EXPAND_SEED_LEN: size_t = 16;
 
-pub fn fill_sapling_seed(zip32_seed: &mut [u8])  {
+pub const ZIP32_SEED_LEN: usize = 32;
+
+pub fn fill_sapling_seed(zip32_seed: &mut [u8; ZIP32_SEED_LEN]) {
     zemu_log_stack("fill_sapling_seed");
-// Get seed from Ed25519
-    let path: [u32;HDPATH_LEN_DEFAULT] = [0x8000002c,
+    // Get seed from Ed25519
+    let path: [u32; HDPATH_LEN_DEFAULT] = [
+        0x8000002c,
         0x80000085,
         MASK_HARDENED,
         MASK_HARDENED,
-        MASK_HARDENED];
+        MASK_HARDENED,
+    ];
 
-    os_perso_derive_node_with_seed_key(
+    super::bindings::os_perso_derive_node_with_seed_key(
         Mode::BIP32,
         Curve::Ed25519,
-        path, HDPATH_LEN_DEFAULT,
+        path,
+        HDPATH_LEN_DEFAULT,
         zip32_seed,
         None,
         None,
-        0
+        0,
     );
-}
-
-
-mod bindings {
-    use super::{bip32::BIP32Path, Curve, Mode};
-    use crate::errors::{catch, Error};
-
-    pub fn os_perso_derive_node_with_seed_key<const B: usize>(
-        mode: Mode,
-        curve: Curve,
-        path: &BIP32Path<B>,
-        out: &mut [u8],
-        chain: Option<&mut [u8; 32]>,
-    ) -> Result<(), Error> {
-        zemu_sys::zemu_log_stack("os_perso_derive_node_with_seed_key\x00");
-        let curve: u8 = curve.into();
-        let mode: u8 = mode.into();
-
-        let out_p = out.as_mut_ptr();
-        let (components, path_len) = {
-            let components = path.components();
-            (components.as_ptr(), components.len() as u32)
-        };
-
-        let chain = if let Some(chain) = chain {
-            chain.as_mut_ptr()
-        } else {
-            std::ptr::null_mut()
-        };
-
-        cfg_if! {
-            if #[cfg(bolos_sdk)] {
-                let might_throw = || unsafe {
-                    crate::raw::os_perso_derive_node_with_seed_key(
-                        mode as _,
-                        curve as _,
-                        components as *const _,
-                        path_len as _,
-                        out_p as *mut _,
-                        chain as *mut _,
-                        std::ptr::null_mut(),
-                        0
-                    )
-                };
-
-                catch(might_throw)?;
-            } else {
-                unimplemented!("os derive called in non-bolos")
-            }
-        }
-
-        Ok(())
-    }
 }
--- bolos-mock/src/crypto/sapling.rs
+++ bolos-mock/src/crypto/sapling.rs
@@ -1,19 +1,18 @@
-pub fn fill_sapling_seed(_zip32_seed: &mut [u8])  {
-    zemu_log_stack("fill_sapling_seed");
-// Get seed from Ed25519
-    let path: [u32;HDPATH_LEN_DEFAULT] = [0x8000002c,
-        0x80000085,
-        MASK_HARDENED,
-        MASK_HARDENED,
-        MASK_HARDENED];
+const MASK_HARDENED: u32 = 0x8000_0000;
 
-    os_perso_derive_node_with_seed_key(
-        Mode::BIP32,
-        Curve::Ed25519,
-        path, HDPATH_LEN_DEFAULT,
-        zip32_seed,
-        None,
-        None,
-        0
-    );}
+const ZIP32_PATH: [u32; 5] = [
+    0x8000002c,
+    0x80000085,
+    MASK_HARDENED,
+    MASK_HARDENED,
+    MASK_HARDENED,
+];
 
+pub const ZIP32_SEED_LEN: usize = 32;
+
+pub fn fill_sapling_seed(zip32_seed: &mut [u8; ZIP32_SEED_LEN]) {
+    let partial_seed: [u8; 5 * 4] = bytemuck::cast(ZIP32_PATH);
+
+    zip32_seed[..5 * 4].copy_from_slice(&partial_seed);
+    zip32_seed[5 * 4..].copy_from_slice(&[0; ZIP32_SEED_LEN - (5 * 4)]);
+}
