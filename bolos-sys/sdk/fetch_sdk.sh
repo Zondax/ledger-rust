d4f60519102297218974eede9dea7a07c06423c6
becominginsane <francesco@zondax.ch>
2023-06-08 20:54:02 +0800

    deps(sdk): update nanos sdk
    deps(sdk): refactor fetch_sdk script

--- bolos-sys/sdk/fetch_sdk.sh
+++ bolos-sys/sdk/fetch_sdk.sh
@@ -4,7 +4,7 @@
 # Nano S (not unified for now)
 : "${BOLOS_SDK_S_PATH:=nanos-secure-sdk}"
 : "${BOLOS_SDK_S_GIT:=https://github.com/LedgerHQ/nanos-secure-sdk}"
-: "${BOLOS_SDK_S_GIT_HASH:=d37bdf1caf98ab2df4b8729c9cb8648ab21cd4b7}"
+: "${BOLOS_SDK_S_GIT_HASH:=ba973f5f4be506e93fe51e1114b9a3ac93adab2a}"
 
 # Unified SDK
 : "${BOLOS_SDK_PATH:=ledger-secure-sdk}"
@@ -12,10 +12,16 @@
 
 # Nano X
 : "${BOLOS_SDK_X_PATH:=nanox-secure-sdk}"
-: "${BOLOS_SDK_X_GIT_TAG:=v1.3.0}"
+: "${BOLOS_SDK_X_GIT_TAG:=v1.5.0}"
+NANOX=(nanox "$BOLOS_SDK_X_PATH" "$BOLOS_SDK_X_GIT_TAG")
 # Nano S+
 : "${BOLOS_SDK_SP_PATH:=nanosplus-secure-sdk}"
-: "${BOLOS_SDK_SP_GIT_TAG:=v1.3.0}"
+: "${BOLOS_SDK_SP_GIT_TAG:=v1.5.0}"
+NANOS2=(nanos2 "$BOLOS_SDK_SP_PATH" "$BOLOS_SDK_SP_GIT_TAG")
+# Stax
+: "${BOLOS_SDK_FS_PATH:=stax-secure-sdk}"
+: "${BOLOS_SDK_FS_GIT_TAG:=v10.1.1}"
+STAX=(stax "$BOLOS_SDK_FS_PATH" "$BOLOS_SDK_FS_GIT_TAG")
 
 echo "Checkout S SDK & update in $BOLOS_SDK_S_PATH from $BOLOS_SDK_S_GIT $BOLOS_SDK_S_GIT_HASH"
 git submodule add --force "$BOLOS_SDK_S_GIT" "$BOLOS_SDK_S_PATH" || true
@@ -28,13 +34,19 @@ popd || exit
 echo "Checkout Unified SDK & update in $BOLOS_SDK_PATH from $BOLOS_SDK_GIT"
 git submodule add --force "$BOLOS_SDK_GIT" "$BOLOS_SDK_PATH" || true
 git submodule update --init --remote "$BOLOS_SDK_PATH"
+setup_unified_sdk() {
+    DEVICE=("${@}")
 
-echo "Create X SDK worktree in $BOLOS_SDK_X_PATH from $BOLOS_SDK_GIT $BOLOS_SDK_X_GIT_TAG"
-git -C "$BOLOS_SDK_PATH" worktree remove -f ../"$BOLOS_SDK_X_PATH"
-git -C "$BOLOS_SDK_PATH" worktree add -f ../"$BOLOS_SDK_X_PATH" "$BOLOS_SDK_X_GIT_TAG"
-echo nanox > "$BOLOS_SDK_X_PATH"/.target
+    TARGET="${DEVICE[0]}"
+    OUT="${DEVICE[1]}"
+    GIT_TAG="${DEVICE[2]}"
 
-echo "Create S+ SDK worktree in $BOLOS_SDK_SP_PATH from $BOLOS_SDK_GIT $BOLOS_SDK_SP_GIT_TAG"
-git -C "$BOLOS_SDK_PATH" worktree remove -f ../"$BOLOS_SDK_SP_PATH"
-git -C "$BOLOS_SDK_PATH" worktree add -f ../"$BOLOS_SDK_SP_PATH" "$BOLOS_SDK_SP_GIT_TAG"
-echo nanos2 > "$BOLOS_SDK_SP_PATH"/.target
+    echo "Create $TARGET worktree in $OUT from $BOLOS_SDK_GIT $GIT_TAG"
+    git -C "$BOLOS_SDK_PATH" worktree remove -f ../"$OUT"
+    git -C "$BOLOS_SDK_PATH" worktree add -f ../"$OUT" "$GIT_TAG"
+    echo "$TARGET" >"$OUT"/.target
+}
+
+setup_unified_sdk "${NANOX[@]}"
+setup_unified_sdk "${NANOS2[@]}"
+setup_unified_sdk "${STAX[@]}"
